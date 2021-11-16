87e3fb0dfb495f52d9c6c07219c2a161198a85c3
ginoongflores <flores.135577@cdo.sti.edu.ph>
2021-11-16 17:37:10 +0800

    added 06 quiz folder

---
+++ src/quiz/Manager.java
@@ -0,0 +1,5 @@
+package quiz;
+
+public class Manager extends Employee {
+    public int budget;
+}
---
+++ src/quiz/pets/Cat.java
@@ -0,0 +1,11 @@
+package quiz.pets;
+
+public class Cat {
+    public Cat() {
+        System.out.println("Cat");
+    }
+
+    public static void main(String[] args) {
+        new Pet(5);
+    }
+}
